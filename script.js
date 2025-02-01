edd7824eadccf9a15212eda7110c101f20fa9386
jhenniifer <chukwumakosisochukwu7gmail.com.gmail.com>
2025-02-01 18:04:33 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function updateTime() {
+    const utcTimeElement = document.querySelector('.utc__time');
+    const now = new Date();
+    utcTimeElement.textContent = now.toUTCString();
+}
+window.onload = updateTime;
